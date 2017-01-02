4617337f3988f13664c8d9673dbe709486931788
roger <rogerortiz4@gmail.com>
2017-01-02 13:33:25 -0600

    add messages nto dashboard

--- static/assets/javascript/index.js
+++ static/assets/javascript/index.js
@@ -2,7 +2,7 @@ $( document ).ready(function() {
 	var js = document.createElement("script");
 
 	js.type = "text/javascript";
-	js.src = "../static/assets/javascript/app/dashboard.js";
+	js.src = "../../static/assets/javascript/app/dashboard.js";
 
 	document.body.appendChild(js);
 
@@ -13,7 +13,16 @@ $( document ).ready(function() {
 
 	$('#editProfile').click(function () {
 		$('.ui.modal').modal('show');
-	}); 
+	});
+
+	$('.message .close')
+	  .on('click', function() {
+	    $(this)
+	      .closest('.message')
+	      .transition('fade')
+	    ;
+	  });
+
 });
 
 $( window ).resize(	function ()	{
